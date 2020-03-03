030c0eabcb5fccd4d22ca5ea0bd8d0eda223380c
kiwigi <43395194+kiwigi@users.noreply.github.com>
2020-03-02 19:34:52 -0700

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,81 +1,58 @@
-// Load up the discord.js library
+//Load up the discord.js library
 const Discord = require("discord.js");
-
-// This is your client. Some people call it `bot`, some people call it `self`, 
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
+//client is client
 const client = new Discord.Client();
-
-// Here we load the config.json file that contains our token and our prefix values. 
+//json file has token and prefix 
 const config = require("./config.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
-
+//mysql
+const mysql = require("mysql");
+//create sql connection
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "420",
+  database: "snake"
+});
+//Runs wen connection is successful
+con.connect(err => {
+    if(err) throw err;
+    console.log("connected to database.");
+});
 client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
+  //This event will run if the bot starts, and logs in, successfully.
+  console.log(`Hisssssss let's spot some snakes`); 
   client.user.setActivity(`Finding snakes`);
 });
 
-client.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild.
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
-
-client.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild.
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
-
-
 client.on("message", async message => {
-  // This event will run on every single message received, from any channel or DM.
-  
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
-  //if(message.author.bot) return;
-  
-  // Also good practice to ignore any message that does not start with our prefix, 
-  // which is set in the configuration file.
-  //if(message.content.indexOf(config.prefix) !== 0) return;
+
 
   if (message.content === 'im a snake') {
     Promise.all([
       message.react('🐍')
     ])
-      .catch(() => console.error('One of the emojis failed to react.'));
-  }
-
-  if (message.content === "I'm a snake") {
-    Promise.all([
-      message.react('🐍')
-    ])
-      .catch(() => console.error('One of the emojis failed to react.'));
+      .catch(() => console.error('failed to react to snake..'));
   }
 
   if (message.author.id  === "96425998089736192") {
     Promise.all([
       message.react('🐍')
     ])
-      .catch(() => console.error('One of the emojis failed to react.'));
+      .catch(() => console.error('failed to react to snake..'));
   }
   
   if (message.author.id  === "478453730082029589") {
     Promise.all([
       message.react('🐍')
     ])
-      .catch(() => console.error('One of the emojis failed to react.'));
+      .catch(() => console.error('failed to react to snake..'));
   }
   
   if (message.author.id  === "521931888378642443") {
     Promise.all([
       message.react('🐍')
     ])
-      .catch(() => console.error('One of the emojis failed to react.'));
+      .catch(() => console.error('failed to react to snake..'));
   }
 
   // Here we separate our "command" name, and our "arguments" for the command. 
@@ -87,39 +64,84 @@ client.on("message", async message => {
 
 
   
-  // Let's go with a few common example commands! Feel free to delete or change those.
+//---------------------------------DETECT COMMAND--------------------------------
   
   if(command === "detect") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("detecting snakes.....");
-    m.edit(`(3) snakes detected!`);
+    con.query(`SELECT * FROM snakepoints`, (err,rows) => {
+    if(err) throw err;
+    
+    let snakeNum = rows.length;
+    m.edit(`${snakeNum} snakes detected!`);
+  });
   }
 
+//---------------------------------EXPOSE COMMAND----------------------------------
+
+if(command === "expose") {
+  con.query(`SELECT * FROM snakepoints`, (err,rows) => {
+  if(err) throw err;
+  message.channel.send(`NAME:\t\t\tSNAKE POINTS:\n`);
+  names = ['Richard','Abner','Tyler','Steven','Alex'];
+  for(i=0;i<rows.length;i++){
+    message.channel.send(`${names[i]}\t\t\t\t${rows[i].points}\n`);
+  }
+  });
+}  
+
+//---------------------------------REPORT COMMAND--------------------------------
+
   if(command === "report") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    //reporting a user adds 1 point to their snake points.
     const m = await message.channel.send("Contacting KRL.....");
+    //if reported user is not mar
     if(message.mentions.users.first().id !== "363189216508903424"){
       m.edit(`I've contacted KRL and the police is on it's way to arrest: <@${message.mentions.users.first().id}>`);
+      //database stuff
+      con.query(`SELECT * FROM snakepoints WHERE id = '${message.mentions.users.first().id}'`, (err,rows) =>{
+        if(err) throw err;
+
+        let sql;
+        //if person has no points, add them to the database
+        if(rows.length < 1) {
+          sql = `INSERT INTO snakepoints (id, points) VALUES ('${message.mentions.users.first().id}', 1)`;
+        //if they have points, add 1 to their points
+        }else {
+          let currentpoints = rows[0].points; 
+          sql = `UPDATE snakepoints SET points = ${currentpoints}+1  WHERE id = '${message.mentions.users.first().id}'`;
+        }
+        con.query(sql, console.log);
+      });
   }
     else{
       m.edit(`<@${message.mentions.users.first().id}> has done nothing wrong.`);
     }}
 
-  if(command === "expose") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-   message.channel.send("Tyler, Richard and Steven are snakes.");
-  }
+
+
+//---------------------------------SNAKEPOINTS COMMAND----------------------------------
+
+if(command === "snakepoints"){
+  let target = message.mentions.users.first() || message.author;
+  con.query(`SELECT * FROM snakepoints WHERE id = '${target.id}'`, (err, rows) => {
+    if(err) throw err;
+
+    if(!rows[0]) return message.channel.send(`This user has no snake points yet! Let's all thank <@${target.id}> for not being a snake.`);
+
+    let tarpoints = rows[0].points;
+    message.channel.send(`This user has:  ${tarpoints} snake points.`);
+  });
+
+}
+
+//--------------------------------HENTAI COMMAND--------------------------------
 
   if(command === "hentai") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     return message.reply("I refuse to enable this kind of behaviour. Please seek help.");
   }
 
-  
+//---------------------------------SAY COMMAND--------------------------------
+
   if(command === "say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
@@ -130,54 +152,8 @@ client.on("message", async message => {
     message.channel.send(sayMessage);
   }
   
-  if(command === "kick") {
-    // This command must be limited to mods and admins. In this example we just hardcode the role names.
-    // Please read on Array.some() to understand this bit: 
-    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    // Let's first check if we have a member and if we can kick them!
-    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-    // We can also support getting the member by ID, which would be args[0]
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    
-    // slice(1) removes the first part, which here should be the user mention or ID
-    // join(' ') takes all the various parts to make it a single string.
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    // Now, time for a swift kick in the nuts!
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+//---------------------------------PURGE COMMAND--------------------------------
 
-  }
-  
-  if(command === "ban") {
-    // Most of this command is identical to kick, except that here we'll only let admins do it.
-    // In the real world mods could ban too, but this is just an example, right? ;)
-    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.bannable) 
-      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
-
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    await member.ban(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-  }
-  
   if(command === "purge") {
     // This command removes all messages from all users in the channel, up to 100.
     
